3eac5d1d189e05fa5f4db6b6ab72787b7f41c3ba
murilofank <murilo.fank@universo.univates.br>
2020-06-18 22:37:27 -0300

    Alterada mensagem de boas vindas da API

--- server/src/controllers/index.controller.ts
+++ server/src/controllers/index.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
 
 export function welcome(req: Request, res: Response): Response {
-   return res.json('Welcome to the Posts API'); 
+   return res.json('Bem vindo Posts API'); 
 }
