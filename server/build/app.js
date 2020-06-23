d065bb530416da11eeac1105144c37089a5ef01a
murilofank <murilo.fank@universo.univates.br>
2020-06-23 20:49:03 -0300

    ajuste .css da navegacao

--- server/build/app.js
+++ server/build/app.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 // Routes
 const index_routes_1 = __importDefault(require("./routes/index.routes"));
 const post_routes_1 = __importDefault(require("./routes/post.routes"));
@@ -33,6 +34,8 @@ class App {
     middlewares() {
         this.app.use(morgan_1.default('dev'));
         this.app.use(express_1.default.json());
+        this.app.use(cors_1.default());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     routes() {
         this.app.use(index_routes_1.default);
